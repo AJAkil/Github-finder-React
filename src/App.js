5ef86f1f01cb2e1b4812c56651b503173c79e272
Akil <cyclosis47@gmail.com>
2020-08-14 19:57:36 +0600

    added alerts, clear button and made lot's of components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./Components/layout/Navbar";
+import Users from "./Components/users/Users";
+import Search from "./Components/users/Search";
+import Alert from "./Components/layout/Alert";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+
+    const response = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      users: response.data,
+      loading: false,
+    });
+  }
+
+  // Search for a single Github user
+  searchUser = async (text) => {
+    this.setState({
+      loading: true,
+    });
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      users: response.data.items,
+      loading: false,
+    });
+  };
+
+  // Clear users from the state
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false,
+    });
+  };
+
+  // Setting up the alert
+  setAlert = (msg, alertType) => {
+    this.setState({
+      alert: { message: msg, type: alertType },
+    });
+
+    // Setting a timeout of 3 Seconds, so that after 3 seconds, the alert
+    // goes away
+    setTimeout(
+      () =>
+        this.setState({
+          alert: null,
+        }),
+      3000
+    );
+  };
+
+  render() {
+    const { loading, users } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUser={this.searchUser}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
